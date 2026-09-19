7110f7a4fb2e4da2a8173966815b1f7448562281
xunwu-top <3393726021@qq.com>
2026-09-19 16:59:54 +0800

    feat(userService):新增获取信息函数

--- app/services/userService.go
+++ app/services/userService.go
@@ -22,11 +22,6 @@ func CheckRegisterUserExists(username string) (bool, error) {
 	return true, nil
 }
 
-func CheckUserExistsByPhoneNum(phoneNum string) error { //登录时用电话号检查用户是否已存在
-	result := database.DB.Where("phone_num = ?", phoneNum).First(&models.User{})
-	return result.Error
-}
-
 // 注册用户，将用户信息存入数据库
 func Register(username string, password string, role string, inviteCode string) (*models.User, error) {
 	var user models.User
@@ -59,4 +54,20 @@ func Register(username string, password string, role string, inviteCode string) 
 	}
 
 	return &user, nil //注册成功
+}
+
+// 登录时用电话号检查用户是否已存在
+func CheckUserExistsByPhoneNum(phoneNum string) error {
+	result := database.DB.Where("phone_num = ?", phoneNum).First(&models.User{})
+	return result.Error
+}
+
+// 获取用户信息
+func GetUserByPhoneNum(phoneNum string) (*models.User, error) {
+	var user models.User
+	result := database.DB.Where("phone_num = ?", phoneNum).First(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
 }
